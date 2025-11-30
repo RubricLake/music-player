18eb6c88541f9abb4381778f9d24f9f26656a05e
Rubric <ethankigotho@gmail.com>
2025-11-29 20:43:39 -0600

    refactor to html5 audio

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,35 +1,18 @@
-
-use std::io::BufReader;
-use std::time::Duration;
-use rodio::Source;
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
+// TODO: Initializes directory if it doesn't exist
 #[tauri::command]
-fn load_audio(path: &str) -> Result<(), String> {
-    let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
-        .map_err(|e| e.to_string())?;
-
-    let mixer = stream_handle.mixer();
-
-    let audio = {
-        let file = std::fs::File::open(path).map_err(|e| e.to_string())?;
-        let sink = rodio::play(mixer, BufReader::new(file)).map_err(|e| e.to_string())?;
-        sink.set_volume(0.5);
-        sink
-    };
-
-    println!("Started Playing: {}", path);
-    audio.sleep_until_end();
-
-    Ok(())
+fn init() {
+    println!("hello world");
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![load_audio])
+        .invoke_handler(tauri::generate_handler![init])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,61 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
+import { useState, useRef } from "react";
+import { convertFileSrc } from "@tauri-apps/api/core";
 import { open } from "@tauri-apps/plugin-dialog";
 
 import "./App.css";
 
+import { SongFile } from "./types";
+
 function App() {
-  const [errText, setErrText] = useState("");
+
+  const [currentSong, setCurrentSong] = useState<SongFile | null>(null);
+  const audioRef = useRef<HTMLAudioElement>(null);
   
-  async function openFile() {
-    const filePath : String | null = await open({
+  const selectFile = async () => {
+    const file = await open({
       multiple: false,
-      directory: false,
-      defaultPath: "E:\\SteamLibrary\\steamapps\\common\\DELTARUNE\\mus",
-      filters: [
-        {
-          name: "Audio Files",
-          extensions: ["mp3", "wav", "flac", "ogg"]
-        }
-      ]
+      director: false,
+      defaultPath: 'E:\\SteamLibrary\\steamapps\\common\\DELTARUNE\\mus',
+      filters: [{name: "Audio", 
+        extensions: ["mp3", "wav", "ogg", "flac"]
+      }]
     });
+    
+    if (file) {
+      const fileSplit : string[] = file.split('\\');
+      const url : string = convertFileSrc(file);
+      const name : string = fileSplit[fileSplit.length - 1];
+      setCurrentSong({ name, url });
 
-    if (!filePath) {
-      console.log("User closed dialog.");
-      return;
-    }
-
-    try {
-      await invoke("load_audio", {path: filePath });
-    } catch (err) {
-      setErrText("Could not load audio :(");
+      console.log(`Name: ${name}`);
+      console.log(`URL: ${url}`);
+      
+      if (audioRef.current) {
+        audioRef.current.playbackRate
+        audioRef.current.load();
+        audioRef.current.play();
+      }
     }
   }
-
+  
   return (
-    <main className="container">
+    <div className="container">
       <h1> Music-Player</h1>
-      <button onClick={openFile}>Choose Song!</button>
-      {errText.length > 0 && <p> {errText} </p>}
-    </main>
+      <button onClick={selectFile}> Choose Song! </button>
+      { currentSong &&
+        <div className="player-box">
+        <p>Now Playing: <strong>{currentSong.name}</strong></p>
+        <audio
+        ref={audioRef}
+        controls
+        src={currentSong.url}
+        style={{width: "100%", marginTop: "20px" }}
+        />  
+      </div>
+      }
+    </div>
   );
+
 }
 
 export default App;
