c5defa15df7483d6e9f68a1396464440f80b9c2f
Rubric <ethankigotho@gmail.com>
2025-11-29 21:06:47 -0600

    customizeable buttons!

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { open } from "@tauri-apps/plugin-dialog";
 
 import "./App.css";
 
-import { SongFile } from "./types";
+import { SongFile, PlayerControl } from "./types";
 
 function App() {
 
@@ -38,6 +38,25 @@ function App() {
     }
   }
   
+  const handleController = (type: PlayerControl) => {
+    if (!audioRef.current) return;
+
+    switch (type) {
+      case PlayerControl.Play:
+        audioRef.current.play();
+        break;
+      case PlayerControl.Pause:
+        audioRef.current.pause();
+        break;
+      case PlayerControl.VolumeUp:
+        audioRef.current.volume += 0.1;
+        break;
+      case PlayerControl.VolumeDown:
+        audioRef.current.volume -= 0.1;
+        break;
+    }
+  }
+  
   return (
     <div className="container">
       <h1> Music-Player</h1>
@@ -45,12 +64,13 @@ function App() {
       { currentSong &&
         <div className="player-box">
         <p>Now Playing: <strong>{currentSong.name}</strong></p>
-        <audio
-        ref={audioRef}
-        controls
-        src={currentSong.url}
-        style={{width: "100%", marginTop: "20px" }}
-        />  
+        <audio ref={audioRef} id="player" src={currentSong.url}></audio>
+        <div className="control-board"> 
+          <button id="play" onClick={ () => handleController(PlayerControl.Play) }>Play</button> 
+          <button id="pause" onClick={ () => handleController(PlayerControl.Pause) }>Pause</button> 
+          <button id="volup" onClick={ () => handleController(PlayerControl.VolumeUp) }>Vol +</button> 
+          <button id="voldown" onClick={ () => handleController(PlayerControl.VolumeDown) }>Vol -</button> 
+        </div>
       </div>
       }
     </div>
--- src/types.ts
+++ src/types.ts
@@ -2,3 +2,10 @@ export interface SongFile {
     name: string,
     url: string 
 }
+
+export enum PlayerControl {
+    Play = 1,
+    Pause,
+    VolumeUp,
+    VolumeDown
+}
